757391674addb7c81ef1841857b274e736d3d95d
Taha Kazmi <96362790+lfgraphics@users.noreply.github.com>
2024-05-11 22:50:58 +0530

    manifest update

---
+++ public/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'Cod Vista';
+
+// Check if `self` is defined (browser environment) before using it
+if (typeof self !== 'undefined') {
+    self.addEventListener('install', (event) => {
+        event.waitUntil(
+            caches.open(CACHE_NAME).then((cache) => {
+                return cache.addAll([
+                    '/',
+                    'logo.png',
+                    'manifest.json',
+                    'beep.mp3',
+                ]);
+            })
+        );
+    });
+
+    self.addEventListener('fetch', (event) => {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            })
+        );
+    });
+}
